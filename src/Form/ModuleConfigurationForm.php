d137ce1f3a5a7fac8004e11a762197811c5a3a0d
Vương Đỗ <bavuong0810@gmail.com>
2023-03-29 12:03:16 +0700

    update config

--- src/Form/ModuleConfigurationForm.php
+++ src/Form/ModuleConfigurationForm.php
@@ -31,26 +31,46 @@ class ModuleConfigurationForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('filerobot.admin_settings');
+
+    $activation = $config->get('activation');
+    $token = $config->get('token');
+    $CNAME = $config->get('CNAME');
+    $SEC = $config->get('SEC');
+    $uploadDirectory = $config->get('uploadDirectory');
+
+    $form['activation'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Activation'),
+      '#default_value' => $activation,
+      '#description' => $this->t('Enable/Disable the Module'),
+    ];
+
     $form['token'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Filerobot token'),
-      '#default_value' => $config->get('token_var'),
-      '#description' => 'Filerobot token from your Filerobot account',
-      '#description_display' => 'after',
+      '#title' => $this->t('Token'),
+      '#default_value' => $token,
+      '#description' => $this->t('Filerobot token from your Filerobot account'),
     ];
-    $form['sectemp'] = [
+
+    $form['CNAME'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('CNAME'),
+      '#default_value' => $CNAME,
+      '#description' => $this->t('Enter the CNAME as per configuration done in your Filerobot Asset Hub interface, once validated and SSL certificate accepted. (Or leave blank if none)'),
+    ];
+
+    $form['SEC'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Security Template Identifier'),
-      '#default_value' => $config->get('sectemp_var'),
-      '#description' => 'To load the Filerobot Widget or Filerobot Image Editor, you you need to create a Security Template in your Filerobot Asset Hub first, in order for your Contenful instantiation of the Filerobot Widget to obtain proper credentials and access your storage',
-      '#description_display' => 'after',
+      '#default_value' => $SEC,
+      '#description' => $this->t('To load the Filerobot Widget or Filerobot Image Editor, you need to create a Security Template in your Filerobot Asset Hub first, in order for your Shopware instantiation of the Filerobot Widget to obtain proper credentials and access your storage.'),
     ];
-    $form['folder'] = [
+
+    $form['uploadDirectory'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Filerobot upload directory'),
-      '#default_value' => $config->get('folder_var'),
-      '#description' => 'The directory in your Filerobot account, where the files will be stored',
-      '#description_display' => 'after',
+      '#default_value' => $uploadDirectory,
+      '#description' => $this->t('The directory in your Filerobot account, where the files will be stored'),
     ];
     return parent::buildForm($form, $form_state);
   }
@@ -58,19 +78,14 @@ class ModuleConfigurationForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) 
+  public function submitForm(array &$form, FormStateInterface $form_state)
   {
-    $folder = $form_state->getValue('folder');
-
-    if (substr($folder, 0, 1) !== '/') 
-    { 
-      $folder = '/' . $folder;
-    }
-
     $this->config('filerobot.admin_settings')
-      ->set('token_var', $form_state->getValue('token'))
-      ->set('sectemp_var', $form_state->getValue('sectemp'))
-      ->set('folder_var', $folder)
+      ->set('token', $form_state->getValue('token'))
+      ->set('SEC', $form_state->getValue('SEC'))
+      ->set('uploadDirectory', $form_state->getValue('uploadDirectory'))
+      ->set('CNAME', $form_state->getValue('CNAME'))
+      ->set('activation', $form_state->getValue('activation'))
       ->save();
 
     parent::submitForm($form, $form_state);
